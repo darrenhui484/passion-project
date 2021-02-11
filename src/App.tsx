1134ab484f696bd8f4a92ba92a559ffbbd7f1ac3
unknown <darrenhui484@gmail.com>
2021-02-10 21:43:47 -0600

    saving

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import React, { ReactElement } from 'react';
-// import logo from './logo.svg';
+import { ReactElement } from 'react';
 import './App.css';
 import DreamPage from './pages/dreams/DreamPage';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App(): ReactElement {
     return (
         <div className="App">
-            <DreamPage />
-            {/* <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header> */}
+            <Router>
+                <Route path="/" component={DreamPage}></Route>
+            </Router>
         </div>
     );
 }
