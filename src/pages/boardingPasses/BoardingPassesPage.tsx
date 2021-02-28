ccdcfda49237659acfb6ee984c9f93167412aa2e
unknown <timmmmy400@gmail.com>
2021-02-27 18:34:11 -0600

    Add boarding passes page css for top half

--- src/pages/boardingPasses/BoardingPassesPage.tsx
+++ src/pages/boardingPasses/BoardingPassesPage.tsx
@@ -8,6 +8,29 @@ function BoardingPassesPage(): ReactElement {
     return (
         <div className="BoardingPasses-page font-aktiv-grotesk">
             <Navbar />
+
+            <div className="BoardingPasses-page-content-container">
+                <div className="BoardingPasses-page-header-container">
+                    <div className="BoardingPasses-page-title-container">
+                        <div className="BoardingPasses-page-title-boarding-text font-saol-display">BOARDING</div>
+                        <div className="BoardingPasses-page-title-bottom-container">
+                            <div className="BoardingPasses-page-title-bottom-left-section">
+                                Us, dreamers stand together. We inspire one another and follow the beat of our own
+                                heart.
+                            </div>
+                            <div className="BoardingPasses-page-title-bottom-right-section font-saol-display">
+                                PASSES
+                            </div>
+                        </div>
+                    </div>
+                    <div className="BoardingPasses-filter-container">
+                        <div className="BoardingPasses-filter-item">FILTER BY:</div>
+                        <div className="BoardingPasses-filter-item">LATEST</div>
+                        <div className="BoardingPasses-filter-item">THEME</div>
+                        <div className="BoardingPasses-filter-item">LOCATION</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
