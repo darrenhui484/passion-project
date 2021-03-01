4fc0834b2cea5634936548f9ba57348faabe5e78
unknown <timmmmy400@gmail.com>
2021-02-28 23:20:54 -0600

    Working filter that has a lot of hardcoded stuff

--- src/pages/boardingPasses/BoardingPassesPage.tsx
+++ src/pages/boardingPasses/BoardingPassesPage.tsx
@@ -11,17 +11,19 @@ function generateDummyData() {
         date: '01.01.21',
         seat: 'PASSION',
         title: 'DEVELOPING COUNTRY',
-        description: 'asdfi ajsdfoi jaweofija woeifj aoei fjao ewi fjs',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
         qrCodeValue: 'test',
     };
 
     const passportProps2 = {
-        countryFrom: 'USA',
+        countryFrom: 'ALB',
         countryTo: 'AUS',
         date: '01.01.21',
         seat: 'PASSION',
         title: 'DEVELOPING COUNTRY',
-        description: 'asdfi ajsdfoi jaweofija woeifj aoei fjao ewi fjs',
+        description:
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,',
         qrCodeValue: 'test',
     };
 
@@ -30,18 +32,20 @@ function generateDummyData() {
         countryTo: 'AUS',
         date: '01.01.21',
         seat: 'PASSION',
-        title: 'DEVELOPING COUNTRY',
-        description: 'asdfi ajsdfoi jaweofija woeifj aoei fjao ewi fjs',
+        title: 'SUSTAINABILITY',
+        description:
+            'quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
         qrCodeValue: 'test',
     };
 
     const passportProps4 = {
-        countryFrom: 'USA',
+        countryFrom: 'AFG',
         countryTo: 'AUS',
         date: '01.01.21',
         seat: 'PASSION',
         title: 'DEVELOPING COUNTRY',
-        description: 'asdfi ajsdfoi jaweofija woeifj aoei fjao ewi fjs',
+        description:
+            'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
         qrCodeValue: 'test',
     };
 
@@ -51,7 +55,8 @@ function generateDummyData() {
         date: '01.01.21',
         seat: 'PASSION',
         title: 'DEVELOPING COUNTRY',
-        description: 'asdfi ajsdfoi jaweofija woeifj aoei fjao ewi fjs',
+        description:
+            '"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.',
         qrCodeValue: 'test',
     };
 
