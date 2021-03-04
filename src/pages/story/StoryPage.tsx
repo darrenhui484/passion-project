38e05d0e1839ead7c3ae340cc2d3cb242c232f93
unknown <timmmmy400@gmail.com>
2021-03-04 00:43:44 -0600

    Mostly? working story page. Does not scale at all, view in 1080 width...

--- src/pages/story/StoryPage.tsx
+++ src/pages/story/StoryPage.tsx
@@ -1,4 +1,5 @@
 import React, { Component, ReactElement } from 'react';
+import Arrow from '../../components/arrow/Arrow';
 import Navbar from '../../components/navbar/Navbar';
 import { IStoryPageProps } from './IStoryPageProps';
 import './StoryPage.scss';
@@ -6,7 +7,7 @@ import './StoryPage.scss';
 const catPic =
     'https://www.thesprucepets.com/thmb/UCGjJ5v6HgPJJ7OOtS5J3ijUBZ8=/1080x1350/filters:fill(auto,1)/35493166_2113126082300521_5592447779063463936_n-5b69b61946e0fb002562c234.jpg';
 
-const pageProps = {
+const dummyProps = {
     title: 'Reclaiming your body',
     author: 'Rachel Holt',
     mission: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
@@ -44,12 +45,17 @@ const pageProps = {
     ],
 } as IStoryPageProps;
 
+interface IStoryPageState {
+    storyPageProps: IStoryPageProps;
+}
+
 // For /story/:id
 // TODO: For now we pass in StoryPageProps, in the future we'll be doing a GET request.
-export default class StoryPage extends Component<IStoryPageProps> {
+export default class StoryPage extends Component<IStoryPageProps, IStoryPageState> {
     constructor(props: IStoryPageProps) {
         super(props);
         console.log('The story id is ', this.props.match.params.storyId);
+        this.state = { storyPageProps: dummyProps };
     }
 
     render(): ReactElement {
@@ -57,7 +63,100 @@ export default class StoryPage extends Component<IStoryPageProps> {
         return (
             <div className="Story-page font-aktiv-grotesk">
                 <Navbar />
-                <div className="Story-page-title-container"></div>
+                <div className="Story-page-content-container">
+                    <div className="Story-page-title-container">
+                        <div className="Story-page-title-left-section">
+                            <div className="Story-page-title-text font-saol-display">
+                                {this.state.storyPageProps.title.toUpperCase()}
+                            </div>
+                            <div className="Story-page-title-left-bottom-section">
+                                <div className="Story-page-title-left-bottom-section-play-button"></div>
+                                <div className="Story-page-title-left-bottom-right-section">
+                                    <div className="Story-page-title-details-container">
+                                        <div className="Story-page-title-details-details">
+                                            <div className="Story-page-title-details-socials">
+                                                <div className="Story-page-title-details-socials-item">
+                                                    <div className="Story-page-title-details-social-title">JOB</div>
+                                                    <div className="Story-page-title-details-social-description">
+                                                        {this.state.storyPageProps.job}
+                                                    </div>
+                                                </div>
+                                                <div className="Story-page-title-details-socials-item">
+                                                    <div className="Story-page-title-details-social-title">SOCIAL</div>
+                                                    <div className="Story-page-title-details-social-description">
+                                                        {this.state.storyPageProps.social}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="Story-page-title-details-mission">
+                                                <div className="Story-page-title-details-social-title">MISSION</div>
+                                                <div className="Story-page-title-details-social-description">
+                                                    {this.state.storyPageProps.mission}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="Story-page-title-details-author">
+                                            WITH {this.state.storyPageProps.author.toUpperCase()}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="Story-page-title-right-section">
+                            <img className="Story-page-title-image" src={this.state.storyPageProps.images[0]} />
+                        </div>
+                    </div>
+                    <div className="Story-page-images">
+                        <img className="Story-page-image-two" src={this.state.storyPageProps.images[1]} />
+                        <img className="Story-page-image-three" src={this.state.storyPageProps.images[2]} />
+                    </div>
+                    <div className="Story-page-text-content-container">
+                        <div className="Story-page-text-content-header">
+                            <div className="Story-page-dummy-right-div"></div>
+                            <div className="Story-page-content-header">{this.state.storyPageProps.header}</div>
+                        </div>
+                        <div className="Story-page-content-inner-container">
+                            <div className="Story-page-content-images-container">
+                                <img className="Story-page-image-four" src={this.state.storyPageProps.images[3]} />
+                                <img className="Story-page-image-five" src={this.state.storyPageProps.images[4]} />
+                            </div>
+                            <div className="Story-page-content-text-container">
+                                <div className="Story-page-content-content-container">
+                                    <div className="Story-page-content-content">
+                                        {this.state.storyPageProps.content}
+                                    </div>
+                                </div>
+                                <div className="Story-page-socials-container">
+                                    {this.state.storyPageProps.socialMedias.map((socialMedia, i) => (
+                                        <a href={socialMedia.url} key={i}>
+                                            <div className="Story-page-socials-item">
+                                                <div className="Story-page-socials-name">
+                                                    {socialMedia.name.toUpperCase()}
+                                                </div>
+                                                <div className="Story-page-socials-arrow">
+                                                    <Arrow />
+                                                </div>
+                                            </div>
+                                        </a>
+                                    ))}
+                                </div>
+                                <div className="Story-page-content-full-conversation font-saol-display">
+                                    FIND{' '}
+                                    <span className="Story-page-content-full-conversation-pill-wrapper">
+                                        <a href={this.state.storyPageProps.socialMedias[0].url}>
+                                            <span className="Story-page-content-full-conversation-pill">
+                                                {this.state.storyPageProps.socialMedias[0].name.toUpperCase()}
+                                            </span>
+                                        </a>
+                                    </span>{' '}
+                                    <span>THE </span> <span>FULL </span>
+                                    <span>CONVERSATION </span>
+                                    <span>ON</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
